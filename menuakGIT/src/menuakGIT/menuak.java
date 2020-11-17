9d94b7b0b5ebf276b04b68daa26b72a85ab4329c
ikaslea <ikaslea@DESKTOP-B54T88L.mshome.net>
2020-11-17 10:20:19 +0100

    menú 4 opciones implementado

--- menuakGIT/src/menuakGIT/menuak.java
+++ menuakGIT/src/menuakGIT/menuak.java
@@ -1,31 +1,42 @@
 package menuakGIT;
 
+import java.util.Scanner;
+
 public class menuak {
 	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("1. Gehiketa" +
+				"2. Kenketa" +
+				"3. Biderketa" +
+				"4. Zatiketa");
 		
-		System.out.println("1. Lehen aukera" +
-				"2. Bigarren aukera" +
-				"3. Hirugarren aukera" +
-				"4. Laugarren aukera");
+		System.out.println("Idatzi bi zenbaki");
+		
+		int z1;
+		int z2;
+		
+		z1 = input.nextInt(); 
+		z2 = input.nextInt(); 
+		
 		
 		int aukerak = 0;
 		switch (aukerak) {
 		
 		case 1:
-			System.out.println("1");
+			System.out.println(z1 + z2);
 			break;
 			
 		case 2:
-			System.out.println("2");
+			System.out.println(z1 - z2);
 			break;
 			
 		case 3:
-			System.out.println("3");
+			System.out.println(z1 * z2);
 			break;
 				
 		case 4:
-			System.out.println("4");
+			System.out.println(z1 / z2);
 			break;
 		
 		}
