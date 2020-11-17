f9f9f28b2e7e632084758655e6d556b1645470cd
ikaslea <ikaslea@DESKTOP-B54T88L.mshome.net>
2020-11-17 10:12:38 +0100

    menu 4 opciones

--- menuakGIT/src/menuakGIT/menuak.java
+++ menuakGIT/src/menuakGIT/menuak.java
@@ -1,5 +1,37 @@
 package menuakGIT;
 
 public class menuak {
-
+	
+	public static void main(String[] args) {
+		
+		System.out.println("1. Lehen aukera" +
+				"2. Bigarren aukera" +
+				"3. Hirugarren aukera" +
+				"4. Laugarren aukera");
+		
+		int aukerak = 0;
+		switch (aukerak) {
+		
+		case 1:
+			System.out.println("1");
+			break;
+			
+		case 2:
+			System.out.println("2");
+			break;
+			
+		case 3:
+			System.out.println("3");
+			break;
+				
+		case 4:
+			System.out.println("4");
+			break;
+		
+		}
+		
+		
+		
+	}
+	
 }
